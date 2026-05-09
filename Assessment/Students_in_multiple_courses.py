a533a28e5a9acb4363069917e00c28d51e5bb777
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-05-09 11:15:39 +0530

    find common students in courses
    This script identifies and prints common students enrolled in multiple courses.

---
+++ Assessment/Students_in_multiple_courses.py
@@ -0,0 +1,11 @@
+course1 = ("Aman", "Riya", "Karan")
+course2 = ("Riya", "Simran", "Aman")
+
+set1 = set(course1)
+set2 = set(course2)
+
+print("Common Students:", set1 & set2)
+
+'''
+output
+Common Students: {'Aman','Riya'}
